5e35ff4f01f57fe440a67f21e3c2eb97778b81f4
Igor Savin <iselwin@gmail.com>
2021-06-25 23:57:04 +0300

    Refactor diffUtils (#24)

---
+++ test/unit/diffUtils.spec.ts
@@ -0,0 +1,107 @@
+import { calculateEntityDiff, calculateJoinTableDiff } from '../../lib/diffUtils'
+
+describe('graphUtils', () => {
+  describe('calculateEntityDiff', () => {
+    it('returns empty result for identical arrays', () => {
+      const array1 = [{ id: 1 }, { id: 2 }]
+      const array2 = [{ id: 2 }, { id: 1 }]
+
+      const result = calculateEntityDiff(array1, array2, 'id')
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for new entries', () => {
+      const array1 = [{ id: 1 }, { id: 2 }]
+      const array2 = [{ id: 2 }, { id: 1 }, { id: 3 }]
+
+      const result = calculateEntityDiff(array1, array2, 'id')
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for removed entries', () => {
+      const array1 = [{ id: 1 }, { id: 2 }, { id: 3 }]
+      const array2 = [{ id: 2 }, { id: 1 }]
+
+      const result = calculateEntityDiff(array1, array2, 'id')
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for both added and removed entries', () => {
+      const array1 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+      const array2 = [{ id: 2 }, { id: 1 }, { id: 5 }, { id: 6 }]
+
+      const result = calculateEntityDiff(array1, array2, 'id')
+
+      expect(result).toMatchSnapshot()
+    })
+  })
+
+  describe('calculateJoinTableDiff', () => {
+    it('returns empty result for identical arrays', () => {
+      const array1 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+        { userId: 3, orgId: 2 },
+      ]
+      const array2 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+        { userId: 3, orgId: 2 },
+      ]
+
+      const result = calculateJoinTableDiff(array1, array2, ['userId', 'orgId'])
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for new entries', () => {
+      const array1 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+      ]
+      const array2 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+        { userId: 3, orgId: 2 },
+      ]
+
+      const result = calculateJoinTableDiff(array1, array2, ['userId', 'orgId'])
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for removed entries', () => {
+      const array1 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+        { userId: 3, orgId: 2 },
+      ]
+      const array2 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+      ]
+
+      const result = calculateJoinTableDiff(array1, array2, ['userId', 'orgId'])
+
+      expect(result).toMatchSnapshot()
+    })
+
+    it('returns correct result for both added and removed entries', () => {
+      const array1 = [
+        { userId: 1, orgId: 1 },
+        { userId: 2, orgId: 1 },
+      ]
+      const array2 = [
+        { userId: 2, orgId: 1 },
+        { userId: 3, orgId: 2 },
+      ]
+
+      const result = calculateJoinTableDiff(array1, array2, ['userId', 'orgId'])
+
+      expect(result).toMatchSnapshot()
+    })
+  })
+})
